3d71a29ee6145119ab34984d2dbd6cbfb71fbfff
DePacifier <37738957+DePacifier@users.noreply.github.com>
2018-04-03 15:16:35 +0300

    Update ThreadClass.py

--- ThreadClass.py
+++ ThreadClass.py
@@ -1,5 +1,6 @@
 from threading import Thread
 import urllib.request
+url = "http://192.168.43.1:33455"
 
 
 class ThreadClass(Thread):
@@ -11,7 +12,7 @@ class ThreadClass(Thread):
     def run(self):
         while True:
             print("Sending Get Request from Thread : ", self.threadIdentifier)
-            recieved_file = urllib.request.urlopen("http://192.168.43.1:33455")
+            recieved_file = urllib.request.urlopen(url)
             print("Request from %s was successful " % recieved_file.geturl())
             ##local_filename, headers = urllib.request.urlretrieve("http://192.168.43.1:33455")
             ##print("Request Successfully stored :", local_filename)
